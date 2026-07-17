b386be01a2a06091198bb70501a0b1b004cc321b
Rami <rami01abdeen@gmail.com>
2026-07-17 21:50:50 +0300

    Add DisplayClientsTable function and create stClientInfo structure

--- ClientManagementSystem/ClientManagementSystem.cpp
+++ ClientManagementSystem/ClientManagementSystem.cpp
@@ -1,4 +1,29 @@
 #include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+struct stClientInfo
+{
+    string AccountNumber;
+    string FirstName;
+    string LastName;
+    string Phone;
+    string Email;
+    string Address;
+    string Country;
+    double Balance;
+};
+
+void DisplayClientsTable()
+{
+    cout << "-----------------------------------------------------------------------------------------------------------\n";
+    cout << "|AccNo.|FName       |LName        |Phone          |Email               |Address   |Country      |Balance  |\n";
+    cout << "-----------------------------------------------------------------------------------------------------------\n";
+    PrintClientsInfo();
+    cout << "-----------------------------------------------------------------------------------------------------------\n";
+}
 
 int main()
 {
